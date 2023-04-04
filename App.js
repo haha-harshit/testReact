5de9436592bd5f3a613adcf981ef6d7a9217d237
Harshit Kumar <bkharshitbk@gmail.com>
2023-04-04 14:52:57 +0530

    intro to jsx/do read comments in app.js

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "hi i am h1 tag")
-  )
-);
+// this is core of react
+// at core react create elements which is javascript object and then it is rendered to DOM as html
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "hi i am h1 tag")
+//   )
+// );
 
-// const heading = React.createElement("h1", {}, "hey from react!");
+// INTRO to JSX
+// JSX is NOT HTML
+// JSX is HTML-like || XML-like syntax used to creat react elements
+// jsx improves readability of react elements which if we do by core syntax will be complex for large code bases.
+// HOW jsx works?
+// JSX to React.createElement by BABEL, then from react element which is object to HTML element when rendered to DOM
+
+const jsxHeading = <h1>Hey from JSX</h1>;
+
+const heading = React.createElement("h1", {}, "hey from react!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
