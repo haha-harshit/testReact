77b0860d7cb1a817cf445b9ba150b874136520bb
Harshit Kumar <bkharshitbk@gmail.com>
2023-04-04 20:17:33 +0530

    update react-dom to react-dom/client

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // this is core of react
 // at core react create elements which is javascript object and then it is rendered to DOM as html
